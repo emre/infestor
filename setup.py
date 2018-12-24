86755250e4190608d56b1aba36604d11563770af
Emre Yilmaz <mail@emreyilmaz.me>
2018-12-24 11:22:26 +0300

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infestor',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='http://github.com/emre/transmitter',
     license='MIT',
